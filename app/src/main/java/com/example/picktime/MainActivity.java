5e5bb1f07c760ddb3f51cfb7a26b5d2a650e156d
mecry makwarela <makwarelamercykgaogelo@gmail.com>
2021-02-18 02:39:04 +0200

    Time

---
+++ app/src/main/java/com/example/picktime/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.picktime;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void showDatePicker(View view) {
+        DialogFragment newFragment;
+        newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(),"timePicker");
+    }
+
+    public void processTimePickerResult(int hour, int minute) {
+        String hour_string = Integer.toString(hour);
+        String minute_string = Integer.toString(minute);
+        String timeMessage = (hour_string +
+                "/" + minute_string);
+
+
+
+        Toast.makeText(this, "Date: " + timeMessage,
+                Toast.LENGTH_SHORT).show();
+    }
+}
